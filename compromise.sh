2fe43f780736f16b8e9ab2745cf3d2c751a029ec
Niklas Rosencrantz <niklasr@protonmail.com>
2022-11-11 19:13:33 +0100

    Create compromise.sh

---
+++ compromise.sh
@@ -0,0 +1,13 @@
+cp copyOfAttack.c attack.c
+rm -f attack_tmp*
+./generate-attack-array < attack.c > attack-array.h
+sed -i 's:#include://#include:g' attack.c
+cat attack.c >> attack-array.h 
+mv attack-array.h attack.c
+./generate-attack-array < attack.c > attack-array.h
+sed -i 's:r compile_attack:r xx_compile_attack:g' attack.c
+cat attack.c >> attack-array.h 
+mv attack-array.h attack.c
+make clean
+make
+sudo make install
